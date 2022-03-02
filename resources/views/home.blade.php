0eeb324d4c50e359db25fe55634f7ac5e8a49b05
dolamm <dolamm1509@gmail.com>
2022-03-02 10:55:32 +0700

    meomeo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container mx-auto sm:px-6 lg:px-8">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -15,9 +15,12 @@
                     @endif
 
                     {{ __('You are logged in!') }}
+                    </br>
+                    <a href="{{route('tc')}}">Trang chủ</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<a href="{{route('posts.create')}}">Create new post</a>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,20 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('auth/login');
-});
-
 Auth::routes();
+Route::get('/', function () {
+    return view('welcome');
+})->name('tc');
+Route::get('/viewpost',function(){
+    return redirect()->route('posts.index');
+})->name('view');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/store',[App\Http\Controllers\PostController::class, 'store'])->name('posts.store');
+Route::get('/index',[App\Http\Controllers\PostController::class, 'index'])->name('posts.index');
+Route::get('/create',[App\Http\Controllers\PostController::class, 'create'])->name('posts.create');
+Route::get('/show/{post}',[App\Http\Controllers\PostController::class, 'show'])->name('posts.show');
+
+Route::post('/CommentStore',[App\Http\Controllers\CommentController::class, 'store'])->name('comments.store');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
