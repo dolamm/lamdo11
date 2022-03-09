1baa0966d628d2a5ee477288093b382d9544775a
Yami Sora <75100194+yamisora-sys@users.noreply.github.com>
2022-03-09 10:30:37 +0700

    Update DislikeComponent.vue
    fix bug

--- resources/js/components/DislikeComponent.vue
+++ resources/js/components/DislikeComponent.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <p id="success"></p>
-       <a href="http://"><i @click.prevent="disLikePost" class="fa fa-thumbs-down" aria-hidden="true"></i>{{ totalDislike }}</a>
+       <span><i style = "cursor:pointer" @click.prevent="disLikePost" class="fa fa-thumbs-down" aria-hidden="true"></i><small>{{ totalDislike }}</small></span>
     </div>
 </template>
 <script>
